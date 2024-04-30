cb63c695f5b8902b3af499c307ff32472ae8183d
Zahid Afsar <zahidtempz@icloud.com>
2024-04-30 11:46:26 -0700

    eod 2

--- src/app/Components/NavBarComponent.tsx
+++ src/app/Components/NavBarComponent.tsx
@@ -4,8 +4,13 @@
 import { Avatar, Dropdown, Navbar } from "flowbite-react";
 import image from '../Assets/image.png'
 import otherImage from '../Assets/image (2).png'
+import logout from '../Assets/image (3).png'
+import { useRouter } from "next/navigation";
 
 export function NavBarComponent() {
+
+    const router = useRouter();
+
   return (
     <Navbar fluid rounded className="gradient h-20 d-flex justify-content-between align-items-center">
     <Navbar.Brand>
@@ -13,15 +18,19 @@ export function NavBarComponent() {
     </Navbar.Brand>     
       <div className="flex md:order-2">
         <Dropdown
-        className="w-28"
+        className="w-36"
           arrowIcon={false}
           label={
             <Avatar alt="User settings" img={otherImage.src} className="h-16 w-16" rounded />
           }
         >
-          <Dropdown.Item className="flex w-28 p-2 hammer justify-center" >PROFILE</Dropdown.Item>
-          <Dropdown.Item className="flex w-28 py-2 hammer justify-center" >CREATE BOARD</Dropdown.Item>
-          <Dropdown.Item className="flex w-28 p-2 hammer justify-center" >LOG OUT</Dropdown.Item>
+          <Dropdown.Item  onClick={() => router.push('/Profile')} className="flex w-36 text-black p-2 hammer justify-center" >PROFILE</Dropdown.Item>
+          <Dropdown.Item  onClick={() => router.push('/Boardpage')}  className="flex w-36 text-[#0B7D61] py-2 hammer justify-center" >CREATE BOARD</Dropdown.Item>
+          <div className="flex mb-2 mx-auto " >
+          <Dropdown.Item  onClick={() => router.push('/')}  className="flex w-36 text-[#CD0000] p-2 hammer justify-center my-auto" >LOG OUT
+          <img src={logout.src} alt="Logout png" className="w-6 items-center my-auto" />
+          </Dropdown.Item>
+          </div>
         </Dropdown>
       </div>
   </Navbar>
--- src/app/Profile/page.tsx
+++ src/app/Profile/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { NavBarComponent } from "../Components/NavBarComponent";
 import  PfpPic  from '../Assets/image (2).png'
+import plus from '../Assets/image (4).png'
 
 export default function Home() {
 
@@ -15,11 +16,23 @@ export default function Home() {
     <NavBarComponent/>
     </div>
     <div className="grid grid-cols-12">
-        <div className="col-span-4 flex justify-center items-center flex-col"> 
+        <div className="col-span-4 flex justify-center items-center flex-col pt-16"> 
           <img className="w-80 mb-4" src={PfpPic.src} alt="Profile Pic" /> 
-          <h1 className="hammer text-2xl text-center">USERNAME</h1>
+          <h1 className="hammer text-black text-4xl text-center">USERNAME</h1>
+          <h1 className="hammer text-2xl text-center pt-4">Joined 12/31/1937</h1>
         </div>
         <div className="col-span-8"> 
+        <div className="h-96 w-full" >
+            <div className="w-full border justify-center" >
+        <div className="flex justify-center items-center pt-16">
+            <h1 className="hammer text-4xl text-center pr-6 pt-1">MY BOARDS</h1>
+            <img src={plus.src} alt="plus png" className="w-6 h-6" />
+        </div>
+        <div className="h-12 pl-2 items-center bg-[#AEE6D9] w-96 justify-center" >
+            <h1 className="text-2xl text-black hammer" >YOUR BOARD</h1>
+        </div>
+        </div>
+        </div>
         </div>
       </div>
     </div>
