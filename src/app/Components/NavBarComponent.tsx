7358be79ce0f880e85994440d64b72a770df915d
Zahid Afsar <zahidtempz@icloud.com>
2024-04-30 11:51:01 -0700

    eod

--- src/app/Components/NavBarComponent.tsx
+++ src/app/Components/NavBarComponent.tsx
@@ -16,7 +16,6 @@ export function NavBarComponent() {
     <Navbar.Brand>
       <img src={image.src} className="ml-3 h-16" alt="Flowbite React Logo" />
     </Navbar.Brand>     
-<<<<<<< HEAD
       <div className="flex md:order-2">
         <Dropdown
         className="w-36"
@@ -34,29 +33,6 @@ export function NavBarComponent() {
           </div>
         </Dropdown>
       </div>
-=======
-    <div className="flex md:order-2">
-        <Dropdown
-          arrowIcon={false}
-          inline
-          label={
-            <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
-          }
-        >
-          <Dropdown.Header>
-            <span className="block text-sm">Bonnie Green</span>
-            <span className="block truncate text-sm font-medium">name@flowbite.com</span>
-          </Dropdown.Header>
-          <Dropdown.Item>Dashboard</Dropdown.Item>
-          <Dropdown.Item>Settings</Dropdown.Item>
-          <Dropdown.Item>Earnings</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item>Sign out</Dropdown.Item>
-        </Dropdown>
-        <Navbar.Toggle />
-      </div>
-      
->>>>>>> 4af35ab (cards)
   </Navbar>
   );
 }
