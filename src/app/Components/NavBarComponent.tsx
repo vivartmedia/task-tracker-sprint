5b863950d9d50cc4bb03558cefea401bec73db40
Zahid Afsar <zahidtempz@icloud.com>
2024-05-02 12:21:20 -0700

    eod 4

--- src/app/Components/NavBarComponent.tsx
+++ src/app/Components/NavBarComponent.tsx
@@ -1,38 +1,53 @@
-
 "use client";
 
-import { Avatar, Dropdown, Navbar } from "flowbite-react";
-import image from '../Assets/image.png'
-import otherImage from '../Assets/image (2).png'
-import logout from '../Assets/image (3).png'
-import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import Image from "next/image";
+import Logo from "../Assets/image.png";
+import Link from "next/link";
+import SignOut from "../Assets/image (3).png";
+import { Dropdown } from "flowbite-react";
+import { INavbar } from "../Interfaces/Interfaces";
 
-export function NavBarComponent() {
-
-    const router = useRouter();
-
+export const NavBarComponent = ({ accountColor }: INavbar) => {
   return (
-    <Navbar fluid rounded className="gradient h-24 d-flex justify-content-between align-items-center">
-    <Navbar.Brand>
-      <img src={image.src} className="ml-3 h-20 pb-3" alt="Flowbite React Logo" />
-    </Navbar.Brand>     
-      <div className="flex md:order-2 border-none">
-        <Dropdown
-        className="w-36 borderSolid bg-transparant"
-          arrowIcon={false}
-          label={
-            <Avatar alt="Nav Bar Options" img={otherImage.src} className="h-16 w-16  bg-none" rounded />
-          }
-        >
-          <Dropdown.Item  onClick={() => router.push('/Profile')} className="flex w-36 text-black p-2 hammer justify-center" >PROFILE</Dropdown.Item>
-          <Dropdown.Item  onClick={() => router.push('/Boardpage')}  className="flex w-36 text-[#0B7D61] py-2 hammer justify-center" >CREATE BOARD</Dropdown.Item>
-          <div className="flex mb-2 mx-auto " >
-          <Dropdown.Item  onClick={() => router.push('/')}  className="flex w-36 text-[#CD0000] p-2 hammer justify-center items-center my-auto" >LOG OUT
-          <img src={logout.src} alt="Logout png" className="w-6 items-center my-auto pb-3" />
-          </Dropdown.Item>
-          </div>
-        </Dropdown>
-      </div>
-  </Navbar>
+    <nav
+      className={`h-24 bg-gradient-to-r from-[#AEE6D9] to-[#3EBE9F] flex justify-between items-center px-5 hammer font-bold`}
+    >
+      <Image src={Logo} alt="Maddie Logo" height={80} className="pb-3" />
+      <Dropdown
+        label="dropdown"
+        dismissOnClick={false}
+        renderTrigger={() => (
+          <span
+            className={`${
+              accountColor != null ? "block" : "hidden"
+            } border borderSolid rounded-full h-12 w-12 cursor-pointer`}
+            style={{
+              background: accountColor?.startsWith("#")
+                ? accountColor
+                : `url(${accountColor})`,
+              backgroundPosition: "center",
+              backgroundSize: "contain",
+            }}
+          ></span>
+        )}
+      >
+        <Dropdown.Item as={Link} href="/Profile">
+          <span className="text-lg ps-7">PROFILE</span>
+        </Dropdown.Item>
+        <Dropdown.Item as={Link} href="/Boardpage">
+          <span className="text-[#0B7D61] text-lg">CREATE BOARD</span>
+        </Dropdown.Item>
+        <Dropdown.Item as={Link} href="/">
+          <span className="text-[#CD0000] text-lg ps-3">LOG OUT</span>{" "}
+          <Image
+            src={SignOut}
+            alt="sign out button pic"
+            height={30}
+            className="pb-1 ps-2"
+          />
+        </Dropdown.Item>
+      </Dropdown>
+    </nav>
   );
-}
+};
