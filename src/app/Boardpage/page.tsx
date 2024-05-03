9c853995266462cfc678153e13ccf2e69235d562
vivartmedia <nanno70@yahoo.de>
2024-05-03 10:00:54 -0700

    container width

--- src/app/Boardpage/page.tsx
+++ src/app/Boardpage/page.tsx
@@ -14,7 +14,7 @@ const Boardpage = () => {
     return (
         <div>
             <NavBarComponent accountColor={accountColor} />
-          
+
 
             <div className="flex min-h-screen flex-col md:p-24 p-6  bg-green-50">
                 <div className="flex md:flex-row flex-col md:items-center pb-4">
@@ -113,7 +113,7 @@ const Boardpage = () => {
 
                     </div>
 
-
+                    {/* <div className="container mx-auto p-4 bg-green-200 max-w-screen-xl" style={{ maxWidth: '90%' }}> */}
                     <div className="container mx-auto p-4  m-10  bg-green-300	max-w-screen-xl" style={{ maxWidth: '90%' }}>
                         <h1 className="pb-4 text-2xl font-semibold">In Progress</h1>
                         <div className="flex items-center space-y-4 md:space-y-0 md:flex-row flex-col align-middle md:justify-around md:pb-4 md:overflow-x-auto overflow-y-scroll max-h-96 md:space-x-4 ">
@@ -173,87 +173,86 @@ const Boardpage = () => {
 
 
 
-
-                    <div className="container mx-auto p-4  m-10  bg-green-400	max-w-screen-xl" style={{ maxWidth: '90%' }}>
-
-
-
-                    <div className="container mx-auto p-4  m-10  bg-green-400	max-w-screen-xl" style={{ maxWidth: '90%' }}>
-                        <h1 className="pb-4 text-2xl font-semibold">Completed</h1>
-                        <div className="flex items-center space-y-4 md:space-y-0 md:flex-row flex-col align-middle md:justify-around md:pb-4 md:overflow-x-auto overflow-y-scroll max-h-96 md:space-x-4 ">
+                   
 
 
 
-                            <Card href="#" className="flex-none max-w-sm bg-white p-4 md:w-80">
-                                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                    Test login
-                                </h5>
-                                <p className="font-normal text-gray-700 dark:text-gray-400">
-                                    A short description of the task you are viewing....
-                                </p>
-                                <div className="flex justify-between">
-                                    <h2 className="text-lg font-semibold	">Priority -2</h2>
-                                    <div className="w-12 h-12 bg-red-500 rounded-full"></div>
-                                </div>
-                            </Card>
-                            <Card href="#" className="flex-none max-w-sm bg-white p-4 md:w-80">
-                                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                    Test login
-                                </h5>
-                                <p className="font-normal text-gray-700 dark:text-gray-400">
-                                    A short description of the task you are viewing....
-                                </p>
-                                <div className="flex justify-between">
-                                    <h2 className="text-lg font-semibold	">Priority -2</h2>
-                                    <div className="w-12 h-12 bg-gray-300 rounded-full"></div>
-                                </div>
-                            </Card>
-                            <Card href="#" className="flex-none max-w-sm bg-white p-4 md:w-80">
-                                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                    Test login
-                                </h5>
-                                <p className="font-normal text-gray-700 dark:text-gray-400">
-                                    A short description of the task you are viewing....
-                                </p>
-                                <div className="flex justify-between">
-                                    <h2 className="text-lg font-semibold	">Priority -2</h2>
-                                    <div className="w-12 h-12 bg-blue-400 rounded-full"></div>
-                                </div>
-                            </Card>
-                            <Card href="#" className="flex-none max-w-sm bg-white p-4 md:w-80">
-                                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                    Test login
-                                </h5>
-                                <p className="font-normal text-gray-700 dark:text-gray-400">
-                                    A short description of the task you are viewing....
-                                </p>
-                                <div className="flex justify-between">
-                                    <h2 className="text-lg font-semibold	">Priority -2</h2>
-                                    <div className="w-12 h-12 bg-pink-400 rounded-full"></div>
-                                </div>
-                            </Card>
-                            <Card href="#" className="flex-none max-w-sm bg-white p-4 md:w-80">
-                                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                    Test login
-                                </h5>
-                                <p className="font-normal text-gray-700 dark:text-gray-400">
-                                    A short description of the task you are viewing....
-                                </p>
-                                <div className="flex justify-between">
-                                    <h2 className="text-lg font-semibold	">Priority -2</h2>
-                                    <div className="w-12 h-12 bg-pink-400 rounded-full"></div>
-                                </div>
-                            </Card>
-                        </div>
+                        <div className="container mx-auto p-4  m-10  bg-green-400	max-w-screen-xl" style={{ maxWidth: '90%' }}>
+                            <h1 className="pb-4 text-2xl font-semibold">Completed</h1>
+                            <div className="flex items-center space-y-4 md:space-y-0 md:flex-row flex-col align-middle md:justify-around md:pb-4 md:overflow-x-auto overflow-y-scroll max-h-96 md:space-x-4 ">
+
+
+
+                                <Card href="#" className="flex-none max-w-sm bg-white p-4 md:w-80">
+                                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                        Test login
+                                    </h5>
+                                    <p className="font-normal text-gray-700 dark:text-gray-400">
+                                        A short description of the task you are viewing....
+                                    </p>
+                                    <div className="flex justify-between">
+                                        <h2 className="text-lg font-semibold	">Priority -2</h2>
+                                        <div className="w-12 h-12 bg-red-500 rounded-full"></div>
+                                    </div>
+                                </Card>
+                                <Card href="#" className="flex-none max-w-sm bg-white p-4 md:w-80">
+                                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                        Test login
+                                    </h5>
+                                    <p className="font-normal text-gray-700 dark:text-gray-400">
+                                        A short description of the task you are viewing....
+                                    </p>
+                                    <div className="flex justify-between">
+                                        <h2 className="text-lg font-semibold	">Priority -2</h2>
+                                        <div className="w-12 h-12 bg-gray-300 rounded-full"></div>
+                                    </div>
+                                </Card>
+                                <Card href="#" className="flex-none max-w-sm bg-white p-4 md:w-80">
+                                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                        Test login
+                                    </h5>
+                                    <p className="font-normal text-gray-700 dark:text-gray-400">
+                                        A short description of the task you are viewing....
+                                    </p>
+                                    <div className="flex justify-between">
+                                        <h2 className="text-lg font-semibold	">Priority -2</h2>
+                                        <div className="w-12 h-12 bg-blue-400 rounded-full"></div>
+                                    </div>
+                                </Card>
+                                <Card href="#" className="flex-none max-w-sm bg-white p-4 md:w-80">
+                                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                        Test login
+                                    </h5>
+                                    <p className="font-normal text-gray-700 dark:text-gray-400">
+                                        A short description of the task you are viewing....
+                                    </p>
+                                    <div className="flex justify-between">
+                                        <h2 className="text-lg font-semibold	">Priority -2</h2>
+                                        <div className="w-12 h-12 bg-pink-400 rounded-full"></div>
+                                    </div>
+                                </Card>
+                                <Card href="#" className="flex-none max-w-sm bg-white p-4 md:w-80">
+                                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                        Test login
+                                    </h5>
+                                    <p className="font-normal text-gray-700 dark:text-gray-400">
+                                        A short description of the task you are viewing....
+                                    </p>
+                                    <div className="flex justify-between">
+                                        <h2 className="text-lg font-semibold	">Priority -2</h2>
+                                        <div className="w-12 h-12 bg-pink-400 rounded-full"></div>
+                                    </div>
+                                </Card>
+                            </div>
+
+
+                        
 
 
                     </div>
-
-
                 </div>
             </div>
         </div>
-</div>
     )
 }
 
